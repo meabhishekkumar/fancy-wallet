a6c210011d38010ea987684949530d4595a9fcef
meabhishekkumar <kumar.abhishek@berkeley.edu>
2021-12-11 15:41:11 +0530

    updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="ak-fancywallet",
     packages=find_packages(),
     install_requires=["click", "requests"],
-    version="0.0.0",
+    version="0.0.1",
     entry_points="""
     [console_scripts]
     ak-fancywallet=fancywallet:fancywallet
